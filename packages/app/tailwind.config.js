3ba0de8002cad24d74d2a0a4b603b25aeeb7bd57
corentin-glt <gillot.corentin@gmail.com>
2021-08-22 09:49:27 +0200

    app refacto

---
+++ packages/app/tailwind.config.js
@@ -0,0 +1,11 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  purge: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {},
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+};
